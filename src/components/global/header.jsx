100af1a81b8c46d11b39c673d37ad175fc688475
Joel Krause <35904580+joelkrause@users.noreply.github.com>
2021-07-04 01:05:44 +1000

    Update header.jsx

--- src/components/global/header.jsx
+++ src/components/global/header.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Link } from "gatsby"
-import styled from 'styled-components';
-import { breakpointFrom, breakpointTo, breakpoints } from './StyledBreakpoints';
 import Memoji from './memoji'
 
 const Header = () => {

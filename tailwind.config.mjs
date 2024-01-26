0c1cb010aa3cff9697bdea199e6afafd5c58c062
joelkrause <joel.krause8@gmail.com>
2024-01-26 22:24:16 +1100

    astro

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: true,
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Martian Mono", "monospace"],
+      },
+    },
+  },
+  plugins: [],
+};

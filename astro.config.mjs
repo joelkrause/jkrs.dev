a64d0c19d44d54efd7e964905cae476aa10a182e
joelkrause <joel.krause8@gmail.com>
2024-01-27 16:04:12 +1100

    feat: init storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
 import { defineConfig } from "astro/config";
-
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
 import tailwind from "@astrojs/tailwind";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        home: "components/storyblok/templates/home",
+      },
+    }),
+  ],
 });
